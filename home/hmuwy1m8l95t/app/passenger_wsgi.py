f09ef298dc467217dfa5a65c69aeee85b57a443a
hossam <hossam@hossam.com>
2021-12-27 13:03:52 +0200

    2 ** 27-12-2021 ** 1:00 PM - work

--- home/hmuwy1m8l95t/app/passenger_wsgi.py
+++ home/hmuwy1m8l95t/app/passenger_wsgi.py
@@ -7,7 +7,7 @@ sys.path.insert(0, os.path.dirname(__file__))
 
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    message = 'It works! ya hossam\n'
+    message = 'It works! ya hossam at 27-12-2021\n'
     version = 'Python %s\n' % sys.version.split()[0]
     response = '\n'.join([message, version])
     return [response.encode()]
